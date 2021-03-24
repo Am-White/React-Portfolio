611a3fb3667cb3f24ca66231cc21189355432e04
littlefire <aybwhite@gmail.com>
2021-03-23 17:37:56 -0700

    added resumeData- datafor entire portfolio and links

---
+++ react-portfolioo/src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"",
+    "name": "Amethyst White",
+    "role": "Software Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "roleDescription": "I like...",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/amethyst-white-1942761b7/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/Am-White",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am...",
+    "address":"United States",
+    "website":"",
+    "education":[
+      {
+        "UniversityName":"",
+        "specialization":"",
+        "MonthOfPassing":"",
+        "YearOfPassing":"",
+        "Achievements":""
+      },
+      {
+        "UniversityName":"",
+        "specialization":"",
+        "MonthOfPassing":"",
+        "YearOfPassing":"",
+        "Achievements":""
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"",
+        "specialization":"",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":""
+      },
+      {
+        "CompanyName":"",
+        "specialization":"",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"",
+        "Achievements":""
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"",
+        "description":"",
+        "imgurl":""
+      },
+      {
+        "name":"",
+        "description":"",
+        "imgurl":""
+      },
+      {
+        "name":"",
+        "description":"",  
+        "imgurl":""
+      },
+      {
+        "name":"",
+        "description":"",
+        "imgurl":""
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
